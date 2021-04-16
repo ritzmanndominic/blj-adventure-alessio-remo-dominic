64543156d76c8f7f576708942361964ecdfe8db4
xxD04xx <dominic.ritzmann@noseryoung.ch>
2021-04-16 10:41:57 +0200

    java: enum created

---
+++ src/com/company/Direction.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public enum Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST;
+
+    public static final int NOEXIT = -1;
+}
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,19 +1,28 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Room {
+    private String name;
     private int width;
     private int height;
     private ArrayList<Items> itemsArrayList = new ArrayList<>();
     private ArrayList<Room> connector = new ArrayList<>();
+    private ArrayList<String> commands = new ArrayList<>(Arrays.asList("north", "east", "south", "west"));
 
     public Room(){}
 
-    public Room(int width, int height, ArrayList<Items> itemsArrayList) {
+    public Room(String name, int width, int height, ArrayList<Items> itemsArrayList) {
         this.width = width;
         this.height = height;
         this.itemsArrayList = itemsArrayList;
         this.connector = new ArrayList<Room>();
+
+
+        //connector.add(new Room("Office", 20, 20, Direction.NORTH, Direction.NOEXIT, Direction.NOEXIT, Direction.WEST ))
     }
+
+
+
 }
