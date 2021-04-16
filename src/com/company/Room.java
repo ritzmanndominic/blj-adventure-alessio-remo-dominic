467f06aaa136221da3be67575f8a6dcc0f438c0f
xxD04xx <dominic.ritzmann@noseryoung.ch>
2021-04-16 10:53:39 +0200

    java: deleted width and height

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -5,24 +5,18 @@ import java.util.Arrays;
 
 public class Room {
     private String name;
-    private int width;
-    private int height;
     private ArrayList<Items> itemsArrayList = new ArrayList<>();
     private ArrayList<Room> connector = new ArrayList<>();
     private ArrayList<String> commands = new ArrayList<>(Arrays.asList("north", "east", "south", "west"));
 
     public Room(){}
 
-    public Room(String name, int width, int height, ArrayList<Items> itemsArrayList) {
-        this.width = width;
-        this.height = height;
+    public Room(String name, ArrayList<Items> itemsArrayList) {
         this.itemsArrayList = itemsArrayList;
         this.connector = new ArrayList<Room>();
 
 
-        //connector.add(new Room("Office", 20, 20, Direction.NORTH, Direction.NOEXIT, Direction.NOEXIT, Direction.WEST ))
+        //connector.add(new Room("Office", 2, Direction.NOEXIT, Direction.NOEXIT, 1 ));
     }
-
-
 
 }
