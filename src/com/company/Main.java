de3ef81c0a30b9d2dc591e6fab0bd9748f8009f5
alessoi1 <69907003+alessoi1@users.noreply.github.com>
2021-04-21 11:50:03 +0200

    java: store location

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         IO io = new IO();
 
         Game game = new Game();
@@ -13,11 +13,13 @@ public class Main {
         game.createDoors(game.getRooms(), game.getDoors());
         game.createItems(game.getItems());
         game.addItems(game.getRooms(), game.getItems());
+        StoreScore.saveObject("bananen", game);
         /*
         io.map();
         io.printBorder("Balcony ");
         game.move(game);
         */
         io.switcher(game, player);
+       // StoreScore.readObject("bananen");
     }
 }
--- src/com/company/StoreScore.java
+++ src/com/company/StoreScore.java
@@ -1,4 +1,30 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class StoreScore {
+
+    public static void saveObject(String fileWriteTo, Game game) throws Exception{
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(game.getRooms().get(game.getActiveRoom()).getName());
+
+        FileOutputStream fos = new FileOutputStream(fileWriteTo);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(arrayList);
+        oos.close();
+    }
+
+    public static void readObject(String fileToReadForm) throws IOException, ClassNotFoundException{
+        FileInputStream fis = new FileInputStream(fileToReadForm);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+        ArrayList<String> arrayList = (ArrayList<String>) ois.readObject();
+        ois.close();
+
+        for (String entry : arrayList){
+            System.out.println(entry);
+        }
+    }
 }
