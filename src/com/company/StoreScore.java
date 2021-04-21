a368f56bdb0c408dcbdf51f6474bbdad98f856d6
alessoi1 <69907003+alessoi1@users.noreply.github.com>
2021-04-21 13:55:52 +0200

    java: store informations

--- src/com/company/StoreScore.java
+++ src/com/company/StoreScore.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 
 public class StoreScore {
 
-    public static void saveObject(String fileWriteTo, Game game) throws Exception{
+    public static void saveObject(String fileWriteTo, Player player1, Game game) throws Exception{
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add(game.getRooms().get(game.getActiveRoom()).getName());
-        for (int i=0; i < game.getItems().size(); i++){
-            arrayList.add(game.getItems().get(i).getName());
+        for (int i=0; i < player1.getItemList().size(); i++){
+            arrayList.add(player1.getItemList().get(i).getName());
         }
 
         FileOutputStream fos = new FileOutputStream(fileWriteTo);
