4566a1edb637be713ea3907c0bbee1b6fbf16835
jannik-boeppli <jannik.boeppli@noseryoung.ch>
2021-05-04 09:42:18 +0200

    java: added new gui

--- blj-adventure-alessio-remo-dominic-Jannik_javaFx/src/sample/Main.java
+++ blj-adventure-alessio-remo-dominic-Jannik_javaFx/src/sample/Main.java
@@ -1,18 +1,31 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
     Gui gui = new Gui();
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-gui.start(primaryStage);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Title");
+        Scene scene = new Scene(root, 760, 680);
+        stage.setMinWidth(760);
+        stage.setMinHeight(680);
+        scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+//
+//        gui.start(primaryStage);
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
